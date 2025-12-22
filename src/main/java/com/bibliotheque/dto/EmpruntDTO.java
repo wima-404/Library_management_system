ba063d0310c6c2f2c579a3105bc4e42f4b86cb03
wiam <blkwiam2003@gmail.com>
2025-12-22 10:00:59 +0100

    EmpruntDTO

--- src/main/java/com/bibliotheque/dto/EmpruntDTO.java
+++ src/main/java/com/bibliotheque/dto/EmpruntDTO.java
@@ -1,4 +1,27 @@
 package com.bibliotheque.dto;
 
+import com.bibliotheque.entity.Emprunt;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmpruntDTO {
+    private Long id;
+    private Long livreId;
+    private String livreTitre;
+    private String livreIsbn;
+    private Long utilisateurId;
+    private String utilisateurNom;
+    private String utilisateurEmail;
+    private LocalDate dateEmprunt;
+    private LocalDate dateRetourPrevue;
+    private LocalDate dateRetourEffective;
+    private Emprunt.StatutEmprunt statut;
+    private Boolean prolonge;
 }
+
